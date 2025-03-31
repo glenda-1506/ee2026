8acd9888a7ec85bb7cfb67fc88120a6971653f88
C2linaung <c2linaung.main@gmail.com>
2025-03-31 13:19:55 +0800

    refactor ready lines

--- FDP.srcs/sources_1/new/combined_gate.v
+++ FDP.srcs/sources_1/new/combined_gate.v
@@ -63,17 +63,10 @@ module combined_gate#(
     input [GATE_OUTPUT_ID_BIT-1:0] g5_out_id_in,
     
     // Outputs from variable circuit segments
-    output var_ready_0,
-    output var_ready_1,
-    output var_ready_2,
+    output [2:0] var_ready,
     
     // Draw signals and output ids from gate containers
-    output gate_ready_0,
-    output gate_ready_1,
-    output gate_ready_2,
-    output gate_ready_3,
-    output gate_ready_4,
-    output gate_ready_5,
+    output [5:0] gate_ready,
     output [GATE_OUTPUT_ID_BIT-1:0] g0_out_id,
     output [GATE_OUTPUT_ID_BIT-1:0] g1_out_id,
     output [GATE_OUTPUT_ID_BIT-1:0] g2_out_id,
@@ -90,7 +83,7 @@ module combined_gate#(
         .letter_info(0),
         .not_gate_visability(1), // to-change as needed
         .segment_visability(1),  // to-change as needed
-        .draw(var_ready_0)
+        .draw(var_ready[0])
     );
 
     variable_circuit_segment #(DISPLAY_WIDTH, DISPLAY_HEIGHT) B (
@@ -101,7 +94,7 @@ module combined_gate#(
         .letter_info(1),
         .not_gate_visability(1),
         .segment_visability(1),
-        .draw(var_ready_1));
+        .draw(var_ready[1]));
 
     variable_circuit_segment #(DISPLAY_WIDTH, DISPLAY_HEIGHT) C (
         .x_addr(x_index),
@@ -111,7 +104,7 @@ module combined_gate#(
         .letter_info(2),
         .not_gate_visability(1),
         .segment_visability(1),
-        .draw(var_ready_2));
+        .draw(var_ready[2]));
 
     gate_container #(DISPLAY_WIDTH, DISPLAY_HEIGHT) g0 (
         .clk(clk),
@@ -122,7 +115,7 @@ module combined_gate#(
         .input_lines(g0_input_lines),
         .gate_select(g0_gate_type),
         .output_id_in(g0_out_id_in),
-        .draw(gate_ready_0),
+        .draw(gate_ready[0]),
         .output_id(g0_out_id));
 
     gate_container #(DISPLAY_WIDTH, DISPLAY_HEIGHT) g1 (
@@ -134,7 +127,7 @@ module combined_gate#(
         .input_lines(g1_input_lines),
         .gate_select(g1_gate_type),
         .output_id_in(g1_out_id_in),
-        .draw(gate_ready_1),
+        .draw(gate_ready[1]),
         .output_id(g1_out_id));
 
     gate_container #(DISPLAY_WIDTH, DISPLAY_HEIGHT) g2 (
@@ -146,7 +139,7 @@ module combined_gate#(
         .input_lines(g2_input_lines),
         .gate_select(g2_gate_type),
         .output_id_in(g2_out_id_in),
-        .draw(gate_ready_2),
+        .draw(gate_ready[2]),
         .output_id(g2_out_id));
 
     gate_container #(DISPLAY_WIDTH, DISPLAY_HEIGHT) g3 (
@@ -158,7 +151,7 @@ module combined_gate#(
         .input_lines(g3_input_lines),
         .gate_select(g3_gate_type),
         .output_id_in(g3_out_id_in),
-        .draw(gate_ready_3),
+        .draw(gate_ready[3]),
         .output_id(g3_out_id));
 
     gate_container #(DISPLAY_WIDTH, DISPLAY_HEIGHT) g4 (
@@ -170,7 +163,7 @@ module combined_gate#(
         .input_lines(g4_input_lines),
         .gate_select(g4_gate_type),
         .output_id_in(g4_out_id_in),
-        .draw(gate_ready_4),
+        .draw(gate_ready[4]),
         .output_id(g4_out_id));
 
     gate_container #(DISPLAY_WIDTH, DISPLAY_HEIGHT) g5 (
@@ -182,6 +175,6 @@ module combined_gate#(
         .input_lines(g5_input_lines),
         .gate_select(g5_gate_type),
         .output_id_in(g5_out_id_in),
-        .draw(gate_ready_5),
+        .draw(gate_ready[5]),
         .output_id(g5_out_id));
 endmodule
